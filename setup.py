2e9803dde575bc3555f241ba6fcd299d8eaf82de
Dan Tao <daniel.tao@gmail.com>
2017-08-11 08:59:29 -0700

    complete partial description in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='django-fallthrough-cache',
     version='0.1.0',
-    description='A Django cache backend ',
+    description='A Django cache backend with fallthrough',
     long_description=open('README.md').read(),
     author='Dan Tao',
     author_email='daniel.tao@gmail.com',
