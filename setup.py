c170f8ccf51f573ac715e4550e03a827b488dd83
Dan Tao <daniel.tao@gmail.com>
2017-08-11 11:15:59 -0700

    bump version to 0.1.1 after long_description snafu (see 3416240)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-fallthrough-cache',
-    version='0.1.0',
+    version='0.1.1',
     description='A Django cache backend with fallthrough',
     author='Dan Tao',
     author_email='daniel.tao@gmail.com',
