b5a9cb3f4bb440369172d0fd42b630f1c1499954
Dan Tao <daniel.tao@gmail.com>
2017-09-15 16:54:23 -0500

    bump version to 0.1.2 to pick up get_or_set fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-fallthrough-cache',
-    version='0.1.1',
+    version='0.1.2',
     description='A Django cache backend with fallthrough',
     author='Dan Tao',
     author_email='daniel.tao@gmail.com',
