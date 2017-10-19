e3506eb5bd3c47148e0ac64e47c5f2ca8b848298
Dan Tao <daniel.tao@gmail.com>
2017-10-19 10:17:53 -0500

    bump to 0.1.4 for delete/delete_many/clear changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-fallthrough-cache',
-    version='0.1.3',
+    version='0.1.4',
     description='A Django cache backend with fallthrough',
     author='Dan Tao',
     author_email='daniel.tao@gmail.com',
